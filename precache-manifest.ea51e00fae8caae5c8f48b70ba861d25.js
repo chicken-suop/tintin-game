46df776a6c2981f9fd3f48d89fc9a3f34af11ade
ratskin <elliot.schep@gmail.com>
2019-01-30 15:33:04 +0100

    Updates

---
+++ precache-manifest.ea51e00fae8caae5c8f48b70ba861d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b0d99f8ae8e6de2c3a2",
+    "url": "/tintin-game/static/css/main.34f033ee.chunk.css"
+  },
+  {
+    "revision": "6b0d99f8ae8e6de2c3a2",
+    "url": "/tintin-game/static/js/main.6b0d99f8.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/tintin-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b76ab4606760bdf09a73",
+    "url": "/tintin-game/static/js/runtime~main.b76ab460.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tintin-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "93e49a26b919f3d23519719abff76f8d",
+    "url": "/tintin-game/index.html"
+  }
+];
